16ae318e44c711b986fcaacf1469321cabe3a138
avgduck <avgduck@gmail.com>
2025-12-04 04:12:05 -0500

    Initial commit

---
+++ src/Plugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace TourneyInfo;
+
+[BepInPlugin(GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    public const string GUID = "avgduck.plugins.llb.tourneyinfo";
+    public static Plugin Instance { get; private set; }
+    internal static ManualLogSource LogGlobal { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+        LogGlobal = this.Logger;
+    }
+}
